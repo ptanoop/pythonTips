61774841d1343d4e77effb0ee5354546576e1078
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:45:20 +0400

    Create Setup file sharing.py

---
+++ Setup file sharing.py
@@ -0,0 +1,10 @@
+# Python2 
+# python -m SimpleHTTPServer
+
+#Python3
+python3 -m http.server
+
+'''
+  Above commands would start a server on the default port i.e. 8000. 
+  You can also use a custom port by passing it as the last argument to the above commands.
+'''
