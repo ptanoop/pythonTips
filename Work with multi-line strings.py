25c4fde4ef2b6e68e6116de2405c78ce9879b09a
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:33:00 +0400

    Update Work with multi-line strings.py

--- Work with multi-line strings.py
+++ Work with multi-line strings.py
@@ -10,3 +10,14 @@ print(multiStr)
  
 #select * from multi_row 
 #where row_id < 5
+
+
+
+#solution is to split the string into multi lines and enclose the entire string in parenthesis.
+
+multiStr= ("select * from multi_row "
+"where row_id < 5 "
+"order by age") 
+print(multiStr)
+ 
+#select * from multi_row where row_id < 5 order by age
