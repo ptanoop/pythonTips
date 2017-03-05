031a39afc7103b0e765975e0c8a5afdb98ed587e
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:39:21 +0400

    Create Use Interactive _ operator.py

---
+++ Use Interactive _ operator.py
@@ -0,0 +1,12 @@
+'''
+  In the Python console, whenever we test an expression or call a function, 
+  the result dispatches to a temporary name, _ (an underscore).
+'''
+'''
+>>> 2 + 1
+3
+>>> _
+3
+>>> print _
+3
+'''
