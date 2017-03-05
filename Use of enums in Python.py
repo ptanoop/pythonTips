2ad7905cd3a8829d0865e16ac7b6b610e33bae23
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:59:49 +0400

    Create Use of enums in Python.py

---
+++ Use of enums in Python.py
@@ -0,0 +1,13 @@
+class Shapes:
+  Circle, Square, Triangle, Quadrangle = range(4)
+  
+  
+print(Shapes.Circle)
+print(Shapes.Square)
+print(Shapes.Triangle)
+print(Shapes.Quadrangle)
+
+#1-> 0
+#2-> 1
+#3-> 2
+#4-> 3
