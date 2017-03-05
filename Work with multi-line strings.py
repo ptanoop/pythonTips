ee4c77298ec1b8cdd933ae1eea361215d13ea0ab
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:31:21 +0400

    Create Work with multi-line strings.py

---
+++ Work with multi-line strings.py
@@ -0,0 +1,12 @@
+multiStr = "select * from multi_row \
+where row_id < 5"
+print(multiStr)
+ 
+# select * from multi_row where row_id < 5
+
+multiStr = """select * from multi_row 
+where row_id < 5"""
+print(multiStr)
+ 
+#select * from multi_row 
+#where row_id < 5
