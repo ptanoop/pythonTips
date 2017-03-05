b63559a39a565bb880a533be543bf094d4e9f311
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:49:11 +0400

    Create  Simplify if statement.py

---
+++ Simplify if statement.py
@@ -0,0 +1,13 @@
+# To verify multiple values, we can do in the following manner.
+
+'''
+
+if m in [1,3,5,7]:
+
+instead of:
+
+if m==1 or m==3 or m==5 or m==7:
+
+Alternatively, we can use ‘{1,3,5,7}’ instead of ‘[1,3,5,7]’ for ‘in’ operator because ‘set’ can access each element by O(1).
+
+'''
