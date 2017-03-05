6400181f5d06714eab4e0b63ee0dc73eae38a15a
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:36:23 +0400

    Create  Print the file path of imported modules.py

---
+++ Print the file path of imported modules.py
@@ -0,0 +1,8 @@
+import threading 
+import socket
+
+print(threading)
+print(socket)
+
+#1- <module 'threading' from '/usr/lib/python2.7/threading.py'>
+#2- <module 'socket' from '/usr/lib/python2.7/socket.py'>
