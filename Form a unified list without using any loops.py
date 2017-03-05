755d3541317758dfe01e22ea76711986125d412a
Anoop P T <ptanoop@gmail.com>
2017-03-05 20:11:36 +0400

    Update Form a unified list without using any loops.py

--- Form a unified list without using any loops.py
+++ Form a unified list without using any loops.py
@@ -1,5 +1,11 @@
 import itertools
 test = [[-1, -2], [30, 40], [25, 35]]
 print(list(itertools.chain.from_iterable(test)))
- 
 #-> [-1, -2, 30, 40, 25, 35]
+
+
+# pip install more_itertools
+import more_itertools
+test = [[-1, -2], [1, 2, 3, [4, (5, [6, 7])]], (30, 40), [25, 35]]
+print(list(more_itertools.collapse(test)))
+#Output=> [-1, -2, 1, 2, 3, 4, 5, 6, 7, 30, 40, 25, 35]
