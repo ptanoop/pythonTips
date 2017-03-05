2bf64a0445cf585c9e2cb7854064ff1d54c5e66a
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:43:02 +0400

    Create Dictionary_Set comprehensions.py

---
+++ Dictionary_Set comprehensions.py
@@ -0,0 +1,8 @@
+testDict = {i: i * i for i in range(10)} 
+testSet = {i * 2 for i in range(10)}
+
+print(testSet)
+print(testDict)
+
+#set([0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
+#{0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64, 9: 81}
