5e39c8c5f7dfa1d2d24a98685119f963428e7725
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:57:12 +0400

    Create Four ways to reverse string_list.py

---
+++ Four ways to reverse string_list.py
@@ -0,0 +1,18 @@
+# Reverse the list itself.
+testList = [1, 3, 5]
+testList.reverse()
+print(testList)
+#-> [5, 3, 1]
+
+# Reverse while iterating in a loop
+for element in reversed([1,3,5]): print(element)
+#1-> 5
+#2-> 3
+#3-> 1
+
+# Reverse a string in line
+"Test Python"[::-1]
+# nohtyP tseT
+
+# Reverse a list using slicing.
+[1, 3, 5][::-1]
