088ddd40b3713597272350b0421ad9cab529cffe
Anoop P T <ptanoop@gmail.com>
2017-03-05 19:22:16 +0400

    Update In-place swapping of two numbers.py

--- In-place swapping of two numbers.py
+++ In-place swapping of two numbers.py
@@ -5,3 +5,9 @@ print(x, y)
 
 #1 (10, 20)
 #2 (20, 10)
+'''
+  The assignment on the right seeds a new tuple. 
+  While the left one instantly unpacks that (unreferenced) tuple to the names <a> and <b>.
+  Once the assignment is through, the new tuple gets unreferenced and flagged for garbage collection. 
+  The swapping of variables also occurs at eventually.
+'''
